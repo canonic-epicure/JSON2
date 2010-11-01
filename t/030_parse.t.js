f2897bb1cf067ca4d99a8b7a2656c4bc3feb045c
Nickolay Platonov <nickolay@desktop.(none)>
2010-11-01 15:47:06 +0300

    - improved test coverage for parsing

--- t/030_parse.t.js
+++ t/030_parse.t.js
@@ -25,35 +25,30 @@ StartTest(function(t) {
         
         t.ok(JSON2.parse('"foo\\u000abar"') == 'foo\nbar', 'String value parsed ok #4')
         
-        t.ok(JSON2.parse("2")  === 2, 'Number value parsed ok #1')
-        t.ok(JSON2.parse("4.5")  === 4.5, 'Number value parsed ok #2')
+        t.ok(JSON2.parse("2") === 2, 'Number value parsed ok #1')
+        t.ok(JSON2.parse("4.5") === 4.5, 'Number value parsed ok #2')
         
-        t.ok(JSON2.parse("null")  === null, '`null` value parsed ok')
+        t.ok(JSON2.parse("null") === null, '`null` value parsed ok')
         
         
         //======================================================================================================================================================================================================================================================
         t.diag('Array')
         
-        // XXX implement t.isDeeply
+        t.isDeeply(JSON2.parse('[]'), [], 'Empty array parsed ok')
+        t.isDeeply(JSON2.parse('[1,2,3]'), [ 1, 2, 3 ], 'Array parsed ok')
         
-//        t.ok(JSON2.parse([]) == '[]', 'Empty array parsed ok')
-//        t.ok(JSON2.parse([ 1, 2, 3 ]) == '[1,2,3]', 'Array parsed ok')
-//        
-//        
-//        //======================================================================================================================================================================================================================================================
-//        t.diag('Nested array')
-//        
-//        t.ok(JSON2.parse([ [], [] ]) == '[[],[]]', 'Nested array parsed ok #1')
-//        t.ok(JSON2.parse([ [ 1, 2 ], [ 3 ] ]) == '[[1,2],[3]]', 'Nested array parsed ok #2')
-//    
-//        //======================================================================================================================================================================================================================================================
-//        t.diag('Objects')
-//        
-//        t.ok(JSON2.parse({}) == '{}', 'Empty object parsed ok')
-//        t.ok(JSON2.parse({ foo : 'bar', bar : [ 'baz', 'zab' ]}) == '{"foo":"bar","bar":["baz","zab"]}', 'Object parsed ok')
-//        
-//        t.ok(JSON2.parse({ a : undefined }) == '{}', '"Undefined" entry excluded')
         
+        //======================================================================================================================================================================================================================================================
+        t.diag('Nested array')
+        
+        t.isDeeply(JSON2.parse('[[],[]]'), [ [], [] ], 'Nested array parsed ok #1')
+        t.isDeeply(JSON2.parse('[[1,2],[3]]'), [ [ 1, 2 ], [ 3 ] ], 'Nested array parsed ok #2')
+    
+        //======================================================================================================================================================================================================================================================
+        t.diag('Objects')
+        
+        t.isDeeply(JSON2.parse('{}'), {}, 'Empty object parsed ok')
+        t.isDeeply(JSON2.parse('{"foo":"bar","bar":["baz","zab"]}'), { foo : 'bar', bar : [ 'baz', 'zab' ] }, 'Object parsed ok')
         
         t.endAsync(async0)
         
